603dd721235e19ebf1408a2056836c879ca2d630
Blaine Bublitz <phate666x@gmail.com>
2011-09-02 21:00:37 -0700

    Possible solution to video being displayed while Collabo is open and someone joins the hangout.

--- js/hangout_injection.js
+++ js/hangout_injection.js
@@ -7,6 +7,7 @@
 var LAZY_LOAD_MS = 10000;
 
 dojo.declare('HangoutInjection', null,{
+	youtubeIframeId: ':ms',
 	youtubeClassSelector: '.hangout-youtube',
 	youtubeClass: 'hangout-youtube',
 	youtubeNode: null,
@@ -16,6 +17,7 @@ dojo.declare('HangoutInjection', null,{
 	iframeUrl: 'http://tinywebdbjs.appspot.com/embed',
 	uniqueKey: '',
 	videoDisplay: '',
+	videoChild: '',
 	gameDisplay: null,
 	gameNode: null,
 	gameNodeText: 'Collabo',
@@ -42,6 +44,7 @@ dojo.declare('HangoutInjection', null,{
 			dojo.place(gameNode, youtubeNode, 'after');
 			
 			this.videoDisplay = dojo.byId(this.videoId);
+			this.videoChild = dojo.query(this.videoDisplay).children('div:first-child')[0];
 			
 			this.gameDisplay = dojo.create('div', {class: 'CSS_LAYOUT_COMPONENT', style: {display: 'none'}}, this.videoDisplay, 'before');
 			var iframe = dojo.create('iframe', {src: this.iframeUrl + '?hangoutUrl=' + this.uniqueKey, frameBorder: '0', style: {width: '100%', height: '100%'}}, this.gameDisplay, 'first');
@@ -52,45 +55,54 @@ dojo.declare('HangoutInjection', null,{
 	},
 	onGameButtonClick: function() {
 		if (dojo.hasClass(this.youtubeNode, this.toolbarCheckedClass)) {
-			dojo.style(dojo.byId(':mt'), {top: '0', left: '0', width: '0', height: '0'});
+			dojo.style(dojo.byId(this.youtubeIframeId), {top: '0', left: '0', width: '0', height: '0'});
 			dojo.removeClass(this.youtubeNode, this.toolbarCheckedClass);
 		}
-		var videoChild = dojo.query(this.videoDisplay).children('div:first-child')[0];
+		
 		dojo.toggleClass(this.gameNode, this.toolbarCheckedClass);
 		if (dojo.hasClass(this.gameNode, this.toolbarCheckedClass)) { // VISIBLE
 			dojo.query(this.gameDisplay).siblings().forEach(function(sibling){
 				if (dojo.attr(sibling, 'id') != this.videoId) {
 					dojo.style(sibling, {display: 'none'});
+				} else {
+					dojo.style(sibling, {visibility: 'hidden'});
 				}
 			}, this);
 			dojo.style(this.gameDisplay, {display: 'block'});
-			dojo.style(videoChild, {height: '1px', width: '1px'});
+			dojo.style(this.videoChild, {height: '1px', width: '1px'});
 			
 		} else { // NOT VISIBLE
 			dojo.style(this.gameDisplay, {display: 'none'});
-			dojo.style(videoChild, {height: '100%', width: '100%'});
+			dojo.style(this.videoDisplay, {visibility: 'visible'});
+			dojo.style(this.videoChild, {height: '100%', width: '100%'});
 		}
 	},
 	onYoutubeButtonClick: function() {
-		var videoChild = dojo.query(this.videoDisplay).children('div:first-child')[0];
+		//var videoChild = dojo.query(this.videoDisplay).children('div:first-child')[0];
 		dojo.removeClass(this.gameNode, this.toolbarCheckedClass);
 		dojo.style(this.gameDisplay, {display: 'none'});
 		//dojo.style(dojo.byId(':mt'), {display: ''});
-		dojo.query(this.gameDisplay).siblings().forEach(function(sibling){
+		/*dojo.query(this.gameDisplay).siblings().forEach(function(sibling){
 				dojo.style(sibling, {display: 'none'});
-		}, this);
-		dojo.style(videoChild, {height: '1px', width: '1px'});
-		/*if (!dojo.hasClass(this.youtubeNode, this.toolbarCheckedClass)) { // VISIBLE
-			dojo.query(this.gameDisplay).siblings().forEach(function(sibling){
+		}, this);*/
+		//dojo.style(videoChild, {height: '1px', width: '1px'});
+		if (dojo.hasClass(this.youtubeNode, this.toolbarCheckedClass)) { // VISIBLE
+			/*dojo.query(this.gameDisplay).siblings().forEach(function(sibling){
 				if (dojo.attr(sibling, 'id') != ':ms') {
 					dojo.style(sibling, {display: 'none'});
 				}
-			}, this);
-			dojo.style(videoChild, {height: '1px', width: '1px'});
+			}, this);*/
+			dojo.style(this.videoChild, {height: '1px', width: '1px'});
 			
 		} else { // NOT VISIBLE
-			dojo.style(this.gameDisplay, {display: 'none'});
-		}*/
+			//dojo.style(this.gameDisplay, {display: 'none'});
+			dojo.query(this.gameDisplay).siblings().forEach(function(sibling){
+				if (dojo.attr(sibling, 'id') != this.videoId) {
+					dojo.style(sibling, {display: 'none'});
+				}
+			}, this);
+			dojo.style(this.videoChild, {height: '100%', width: '100%'});
+		}
 	}
 })
 
